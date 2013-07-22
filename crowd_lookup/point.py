8013a4be03cc31c3d4e626c2d7093394bb49c0be
averangeall <averangeall@gmail.com>
2013-07-22 22:58:00 +0800

    Add point config file

---
+++ crowd_lookup/point.py
@@ -0,0 +1,31 @@
+# Recomm:
+
+#     how many points will a user query/hate affect a keyword?
+USER_RECOMM_VAL_POINT = 0.9
+
+#     how many points will an admin query/hate affect a keyword?
+ADMIN_RECOMM_VAL_POINT = 4.5
+
+#     how many points required to be visible to everyone?
+MIN_VISIBLE_POINT = 1.5
+
+
+# Explain:
+
+#     how many points will a user like/hate affect a explain?
+USER_EXPL_VAL_POINT = 0.9
+
+#     how many points will an admin like/hate affect a explain?
+ADMIN_EXPL_VAL_POINT = 4.5
+
+#     what is the maximum initial point of a explain from the internet?
+WEB_EXPL_MAX_INIT_POINT = 1.5
+
+#     what is the maximum initial point of a explain from a user?
+USER_EXPL_INIT_POINT = 2.0
+
+#     what is the maximum initial point of a explain from a user?
+ADMIN_EXPL_INIT_POINT = 5.0
+
+
+
